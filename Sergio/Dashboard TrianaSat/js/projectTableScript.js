a66b3471d9bafc8aae59ab7ad80446740cb9e078
sergiosgp90 <sergiosgp90@gmail.com>
2017-02-07 23:25:44 +0100

    Sergio - Tabla de proyectos admin
    Añadidos campos organización y acceso automático a un proyecto, que funciona con un checkbox en cada fila.
    
    Falta ponerle bien el estilo.

--- Sergio/Dashboard TrianaSat/js/projectTableScript.js
+++ Sergio/Dashboard TrianaSat/js/projectTableScript.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var adminLogged = $('.user-dropdown').length > 0;
 
-    $('#content-main .wrapper').load('sections/sectionProjectTable.html', function(){
-        if(!adminLogged){
-           $(".projectActions").addClass("hideToken");
+    $('#content-main .wrapper').load('sections/sectionProjectTable.html', function() {
+        if (!adminLogged) {
+            $(".projectActions").addClass("hideToken");
         }
     });
 
     // Genera un código QR con el token del proyecto seleccionado
     // y lo muestra en un modal
-    $(document).on('click', '.btnToken', function(){
+    $(document).on('click', '.btnToken', function() {
         $('#qrcode').empty();
 
         var token = $(this).attr('token');
@@ -18,13 +18,32 @@ $(document).ready(function(){
         new QRCode("qrcode").makeCode(token);
     });
 
-    $(document).on("change", "input[type=checkbox]", function (e) {
-    e.preventDefault();
+    /*$(document).on('click', '.table-projects tr', function() {
+        if($(this).find('.rememberProject').prop('checked')){
+            $(this).find('.rememberProject').prop('checked', false);
+        }else{
+          $(this).find('.rememberProject').prop('checked', true);
+        }
+    });*/
 
-    if ($(this).is(":checked")) {
-      alert('checked');
-      $("input[type=checkbox]").prop("checked", false);
-      $(this).prop("checked", true);
-    }
-  });
+    // the selector will match all input controls of type :checkbox
+    // and attach a click event handler
+    $(document).on('click', ".rememberProject", function() {
+        // in the handler, 'this' refers to the box clicked on
+        var box = $(this);
+        if (box.is(":checked")) {
+            var group = "input:checkbox[name='" + box.attr("name") + "']";
+            // the checked state of the group/box on the other hand will change
+            // and the current value is retrieved using .prop() method
+            $(group).prop("checked", false);
+            $(group).next("span").css('color', 'red');
+            box.prop("checked", true);
+        } else {
+            $box.prop("checked", false);
+        }
+    });
+
+    $(document).on('click', '#hola', function(){
+      alert($('.rememberProject:checked').attr('paco'));
+    })
 });
