a78367c8e4f6d78e6a66bd9c00c73be1b36b2776
olalas <jcaberera@gmail.com>
2017-02-11 10:51:14 +0100

    graficas dentro web y actualizado solicitud datos a json, falta pedir desde api

--- Jesus/Dashboard TrianaSat/js/chartScriptdos.js
+++ Jesus/Dashboard TrianaSat/js/chartScriptdos.js
@@ -1,74 +1,18 @@
 
-$( function() {
-  alert("collapsible");
-    $( "#accordion" ).accordion({
-      collapsible: true,
-      heightStyle: "content"
-    });
-  } );
+$(document).ready(function(){
 
-$.getJSON("http://www.olalas.hol.es/datosJson/temperaturas.json", function (json) {
+//grafica 1
+$.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
   // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-  var ejex = json.map(function(item) {
+  var altura = json.altura;
+  var ejex = json.altura.map(function(item) {
     return item.hora;
   })
-  var ejeyuno = json.map(function(item){
-    return item.temp2;
-  })
-  var ejeydos = json.map(function(item){
-    return item.temp;
-  })
 
-
-  var data = {
-    labels: ejex,
-    datasets: [
-    {
-      label: "primero",
-      fillColor : "rgba(220,0,220,0.5)",
-      strokeColor : "rgba(0,220,0,1)",
-      pointColor : "rgba(220,0,220,1)",
-      pointStrokeColor : "#ffa",
-      data: ejeyuno
-    },
-    {
-      label: "segundo",
-      fillColor : "rgba(151,187,205,0.5)",
-      strokeColor : "rgba(000,187,205,1)",
-      pointColor : "rgba(100,187,205,1)",
-      pointStrokeColor : "#aff",
-      data : ejeydos
-    }
-
-    ]
-  };
-
-  Chart.defaults.global.animationSteps = 50;
-  Chart.defaults.global.tooltipYPadding = 16;
-  Chart.defaults.global.tooltipCornerRadius = 2;
-  Chart.defaults.global.tooltipTitleFontStyle = "normal";
-  Chart.defaults.global.tooltipFillColor = "rgba(100,160,0,0.8)";
-  Chart.defaults.global.animationEasing = "easeOutBounce";
-  Chart.defaults.global.responsive = true;
-  Chart.defaults.global.scaleLineColor = "black";
-  Chart.defaults.global.scaleFontSize = 16;
-  var ctx = document.getElementById('temperaturas').getContext('2d');
-  var lineDemo = new Chart(ctx).Line(data, {
-
-  });
-
-});
-$.getJSON("http://www.olalas.hol.es/datosJson/altura.json", function (json) {
-  // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-  var ejex = json.map(function(item) {
-    return item.hora;
-  })
-  var ejeyuno = json.map(function(item){
+  var ejeyuno = json.altura.map(function(item){
     return item.metros;
   })
 
-
-alert("ejex grarfica dos");
   var data = {
     labels: ejex,
     datasets: [
@@ -79,19 +23,12 @@ alert("ejex grarfica dos");
       pointColor : "rgba(220,0,220,1)",
       pointStrokeColor : "#ffa",
       data: ejeyuno
-    },
-    {
-      label: "segundo",
-      fillColor : "rgba(151,187,205,0.5)",
-      strokeColor : "rgba(000,187,205,1)",
-      pointColor : "rgba(100,187,205,1)",
-      pointStrokeColor : "#aff",
-      data : ejeydos
     }
 
     ]
   };
 
+  var ctxdos = document.getElementById("grafica1").getContext("2d");
   Chart.defaults.global.animationSteps = 50;
   Chart.defaults.global.tooltipYPadding = 16;
   Chart.defaults.global.tooltipCornerRadius = 2;
@@ -101,9 +38,135 @@ alert("ejex grarfica dos");
   Chart.defaults.global.responsive = true;
   Chart.defaults.global.scaleLineColor = "black";
   Chart.defaults.global.scaleFontSize = 16;
-  var ctx = document.getElementById('myChart2').getContext('2d');
-  var lineDemo = new Chart(ctx).Line(data, {
 
-  });
+  var myChart = new Chart(ctxdos).Line(data);
+});
 
+
+//grafica 2
+
+
+$.getJSON("http://olalas.hol.es/datosJson/temperaturas.json", function (json) {
+  // will generate array with ['Monday', 'Tuesday', 'Wednesday']
+  var altura = json.temperaturas;
+  var ejex = json.temperaturas.map(function(item) {
+    return item.date;
+  })
+
+  var ejeyuno = json.temperaturas.map(function(item){
+    return item.temp;
+  })
+
+  var data = {
+    labels: ejex,
+    datasets: [
+    {
+      label: "primero",
+      fillColor : "rgba(220,0,220,0.5)",
+      strokeColor : "rgba(0,220,0,1)",
+      pointColor : "rgba(220,0,220,1)",
+      pointStrokeColor : "#ffa",
+      data: ejeyuno
+    }
+
+    ]
+  };
+
+  var ctxdos = document.getElementById("grafica2").getContext("2d");
+  Chart.defaults.global.animationSteps = 50;
+  Chart.defaults.global.tooltipYPadding = 16;
+  Chart.defaults.global.tooltipCornerRadius = 2;
+  Chart.defaults.global.tooltipTitleFontStyle = "normal";
+  Chart.defaults.global.tooltipFillColor = "rgba(100,160,0,0.8)";
+  Chart.defaults.global.animationEasing = "easeOutBounce";
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.scaleLineColor = "black";
+  Chart.defaults.global.scaleFontSize = 16;
+
+  var myChart = new Chart(ctxdos).Line(data);
+});
+
+//grafica 3
+
+$.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
+  // will generate array with ['Monday', 'Tuesday', 'Wednesday']
+  var altura = json.altura;
+  var ejex = json.altura.map(function(item) {
+    return item.hora;
+  })
+
+  var ejeyuno = json.altura.map(function(item){
+    return item.metros;
+  })
+
+  var data = {
+    labels: ejex,
+    datasets: [
+    {
+      label: "primero",
+      fillColor : "rgba(220,0,220,0.5)",
+      strokeColor : "rgba(0,220,0,1)",
+      pointColor : "rgba(220,0,220,1)",
+      pointStrokeColor : "#ffa",
+      data: ejeyuno
+    }
+
+    ]
+  };
+
+  var ctxdos = document.getElementById("grafica3").getContext("2d");
+  Chart.defaults.global.animationSteps = 50;
+  Chart.defaults.global.tooltipYPadding = 16;
+  Chart.defaults.global.tooltipCornerRadius = 2;
+  Chart.defaults.global.tooltipTitleFontStyle = "normal";
+  Chart.defaults.global.tooltipFillColor = "rgba(100,160,0,0.8)";
+  Chart.defaults.global.animationEasing = "easeOutBounce";
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.scaleLineColor = "black";
+  Chart.defaults.global.scaleFontSize = 16;
+
+  var myChart = new Chart(ctxdos).Line(data);
+});
+
+//grafica 4
+
+$.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
+  // will generate array with ['Monday', 'Tuesday', 'Wednesday']
+  var altura = json.altura;
+  var ejex = json.altura.map(function(item) {
+    return item.hora;
+  })
+
+  var ejeyuno = json.altura.map(function(item){
+    return item.metros;
+  })
+
+  var data = {
+    labels: ejex,
+    datasets: [
+    {
+      label: "primero",
+      fillColor : "rgba(220,0,220,0.5)",
+      strokeColor : "rgba(0,220,0,1)",
+      pointColor : "rgba(220,0,220,1)",
+      pointStrokeColor : "#ffa",
+      data: ejeyuno
+    }
+
+    ]
+  };
+
+  var ctxdos = document.getElementById("grafica4").getContext("2d");
+  Chart.defaults.global.animationSteps = 50;
+  Chart.defaults.global.tooltipYPadding = 16;
+  Chart.defaults.global.tooltipCornerRadius = 2;
+  Chart.defaults.global.tooltipTitleFontStyle = "normal";
+  Chart.defaults.global.tooltipFillColor = "rgba(100,160,0,0.8)";
+  Chart.defaults.global.animationEasing = "easeOutBounce";
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.scaleLineColor = "black";
+  Chart.defaults.global.scaleFontSize = 16;
+
+  var myChart = new Chart(ctxdos).Line(data);
+});
 });
