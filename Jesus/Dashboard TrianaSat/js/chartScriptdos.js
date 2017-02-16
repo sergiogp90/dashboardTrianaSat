b00d042dd8235daa489ba6c90a999c9eecebff32
olalas <jcabereraj@gmail.com>
2017-02-16 11:00:50 +0100

    graficas metidas

--- Jesus/Dashboard TrianaSat/js/chartScriptdos.js
+++ Jesus/Dashboard TrianaSat/js/chartScriptdos.js
@@ -2,27 +2,37 @@
 $(document).ready(function(){
 
 //grafica 1
-$.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
+var fechaLista =[];
+var altura = [];
+//trianasat2-salesianostriana.rhcloud.com/datossensores
+$.getJSON("http://trianasat2-salesianostriana.rhcloud.com/datossensores", function (json) {
   // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-  var altura = json.altura;
-  var ejex = json.altura.map(function(item) {
-    return item.hora;
-  })
+  var raiz = json._embedded.datossensores;
+//  var altitud = json._embedded.datossensores;
+  console.log(json._embedded.datossensores[0]);
 
-  var ejeyuno = json.altura.map(function(item){
-    return item.metros;
-  })
+  $.each(raiz, function(i, dato) {
+    var fecha = dato.fecha;
+
+    var formattedDate = moment(fecha).format('HH:mm');
+    var altitud = dato.altitud;
+    //var formattedDate = moment(fechaAltura).format('DD/MM/YYYY HH:mm:ss');
+    fechaLista.push(formattedDate);
+    altura.push(parseInt(altitud));
+    ;
+  });
+
 
   var data = {
-    labels: ejex,
+    labels: fechaLista,
     datasets: [
     {
       label: "primero",
-      fillColor : "rgba(220,0,220,0.5)",
-      strokeColor : "rgba(0,220,0,1)",
-      pointColor : "rgba(220,0,220,1)",
-      pointStrokeColor : "#ffa",
-      data: ejeyuno
+      fillColor : "rgba(0,0,200,0.5)",
+      strokeColor : "rgba(0,220,200,1)",
+      pointColor : "rgba(220,100,220,1)",
+      pointStrokeColor : "#a0f",
+      data: altura
     }
 
     ]
@@ -33,7 +43,7 @@ $.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
   Chart.defaults.global.tooltipYPadding = 16;
   Chart.defaults.global.tooltipCornerRadius = 2;
   Chart.defaults.global.tooltipTitleFontStyle = "normal";
-  Chart.defaults.global.tooltipFillColor = "rgba(100,160,0,0.8)";
+  Chart.defaults.global.tooltipFillColor = "rgba(100,160,200,0.8)";
   Chart.defaults.global.animationEasing = "easeOutBounce";
   Chart.defaults.global.responsive = true;
   Chart.defaults.global.scaleLineColor = "black";
@@ -44,21 +54,28 @@ $.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
 
 
 //grafica 2
-
-
-$.getJSON("http://olalas.hol.es/datosJson/temperaturas.json", function (json) {
+$.getJSON("http://trianasat2-salesianostriana.rhcloud.com/datossensores", function (json) {
   // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-  var altura = json.temperaturas;
-  var ejex = json.temperaturas.map(function(item) {
-    return item.date;
-  })
+  var fechaListaD= [];
+  var temperaturaLista = [];
+  var raiz = json._embedded.datossensores;
+//  var altitud = json._embedded.datossensores;
+  console.log(json._embedded.datossensores[0]);
 
-  var ejeyuno = json.temperaturas.map(function(item){
-    return item.temp;
-  })
+  $.each(raiz, function(i, dato) {
+    var fecha = dato.fecha;
+
+    var formattedDate = moment(fecha).format('HH:mm');
+    var temperatura = dato.temperatura;
+    //var formattedDate = moment(fechaAltura).format('DD/MM/YYYY HH:mm:ss');
+    fechaListaD.push(formattedDate);
+    temperaturaLista.push(parseInt(temperatura));
+
+  });
+
 
   var data = {
-    labels: ejex,
+    labels: fechaListaD,
     datasets: [
     {
       label: "primero",
@@ -66,7 +83,7 @@ $.getJSON("http://olalas.hol.es/datosJson/temperaturas.json", function (json) {
       strokeColor : "rgba(0,220,0,1)",
       pointColor : "rgba(220,0,220,1)",
       pointStrokeColor : "#ffa",
-      data: ejeyuno
+      data: temperaturaLista
     }
 
     ]
@@ -88,19 +105,28 @@ $.getJSON("http://olalas.hol.es/datosJson/temperaturas.json", function (json) {
 
 //grafica 3
 
-$.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
+$.getJSON("http://trianasat2-salesianostriana.rhcloud.com/datossensores", function (json) {
   // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-  var altura = json.altura;
-  var ejex = json.altura.map(function(item) {
-    return item.hora;
-  })
+  var fechaListaE= [];
+  var presionLista = [];
+  var raiz = json._embedded.datossensores;
+//  var altitud = json._embedded.datossensores;
 
-  var ejeyuno = json.altura.map(function(item){
-    return item.metros;
-  })
+
+  $.each(raiz, function(i, dato) {
+    var fecha = dato.fecha;
+
+    var formattedDate = moment(fecha).format('HH:mm');
+    var presion = dato.presion;
+    //var formattedDate = moment(fechaAltura).format('DD/MM/YYYY HH:mm:ss');
+    fechaListaE.push(formattedDate);
+    presionLista.push(parseInt(presion));
+
+  });
+
 
   var data = {
-    labels: ejex,
+    labels: fechaListaE,
     datasets: [
     {
       label: "primero",
@@ -108,7 +134,7 @@ $.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
       strokeColor : "rgba(0,220,0,1)",
       pointColor : "rgba(220,0,220,1)",
       pointStrokeColor : "#ffa",
-      data: ejeyuno
+      data: presionLista
     }
 
     ]
@@ -130,19 +156,27 @@ $.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
 
 //grafica 4
 
-$.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
+$.getJSON("http://trianasat2-salesianostriana.rhcloud.com/datossensores", function (json) {
   // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-  var altura = json.altura;
-  var ejex = json.altura.map(function(item) {
-    return item.hora;
-  })
+  var fechaListaA= [];
+  var calidad_aireLista = [];
+  var raiz = json._embedded.datossensores;
+//  var altitud = json._embedded.datossensores;
+  console.log(json._embedded.datossensores[0]);
 
-  var ejeyuno = json.altura.map(function(item){
-    return item.metros;
-  })
+  $.each(raiz, function(i, dato) {
+    var fecha = dato.fecha;
+
+    var formattedDate = moment(fecha).format('HH:mm');
+    var calidad_aire = dato.calidad_aire;
+    //var formattedDate = moment(fechaAltura).format('DD/MM/YYYY HH:mm:ss');
+    fechaListaA.push(formattedDate);
+    calidad_aireLista.push(parseInt(calidad_aire));
+  });
+
 
   var data = {
-    labels: ejex,
+    labels: fechaListaA,
     datasets: [
     {
       label: "primero",
@@ -150,11 +184,12 @@ $.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
       strokeColor : "rgba(0,220,0,1)",
       pointColor : "rgba(220,0,220,1)",
       pointStrokeColor : "#ffa",
-      data: ejeyuno
+      data: calidad_aireLista
     }
 
     ]
   };
+
 
   var ctxdos = document.getElementById("grafica4").getContext("2d");
   Chart.defaults.global.animationSteps = 50;
@@ -162,6 +197,57 @@ $.getJSON("http://olalas.hol.es/datosJson/altura.json", function (json) {
   Chart.defaults.global.tooltipCornerRadius = 2;
   Chart.defaults.global.tooltipTitleFontStyle = "normal";
   Chart.defaults.global.tooltipFillColor = "rgba(100,160,0,0.8)";
+  Chart.defaults.global.animationEasing = "easeOutBounce";
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.scaleLineColor = "black";
+  Chart.defaults.global.scaleFontSize = 16;
+
+  var myChart = new Chart(ctxdos).Line(data);
+});
+
+//grafica 5
+
+$.getJSON("http://trianasat2-salesianostriana.rhcloud.com/datossensores", function (json) {
+  // will generate array with ['Monday', 'Tuesday', 'Wednesday']
+  var fechaListaH= [];
+  var humedadLista = [];
+  var raiz = json._embedded.datossensores;
+//  var altitud = json._embedded.datossensores;
+  console.log(json._embedded.datossensores[0]);
+
+  $.each(raiz, function(i, dato) {
+    var fecha = dato.fecha;
+
+    var formattedDate = moment(fecha).format('HH:mm');
+    var humedad = dato.humedad;
+    //var formattedDate = moment(fechaAltura).format('DD/MM/YYYY HH:mm:ss');
+    fechaListaH.push(formattedDate);
+    humedadLista.push(parseInt(humedad));
+  });
+
+
+  var data = {
+    labels: fechaListaH,
+    datasets: [
+    {
+      label: "primero",
+      fillColor : "rgba(220,40,50,0.5)",
+      strokeColor : "rgba(0,0,50,1)",
+      pointColor : "rgba(220,0,0,1)",
+      pointStrokeColor : "#f0a",
+      data: humedadLista
+    }
+
+    ]
+  };
+
+
+  var ctxdos = document.getElementById("grafica5").getContext("2d");
+  Chart.defaults.global.animationSteps = 50;
+  Chart.defaults.global.tooltipYPadding = 16;
+  Chart.defaults.global.tooltipCornerRadius = 2;
+  Chart.defaults.global.tooltipTitleFontStyle = "normal";
+  Chart.defaults.global.tooltipFillColor = "rgba(100,0,0,0.8)";
   Chart.defaults.global.animationEasing = "easeOutBounce";
   Chart.defaults.global.responsive = true;
   Chart.defaults.global.scaleLineColor = "black";
---
+++ Jesus/Dashboard TrianaSat/js/dashIndexScript.js
@@ -0,0 +1,161 @@
+var actualMenuSelected;
+var isMobile;
+var projectToken;
+
+// Cierra el menú lateral
+function closeSideBar() {
+    $('.sidebar-menu').css('display', 'none');
+}
+
+// Comprueba el tamaño del dispositivo.
+function checkScreenSize() {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+        isMobile = true;
+        closeSideBar();
+    } else {
+        isMobile = false;
+    }
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+
+    /* TODO activar cuando esté alojada en el dominio, cambiando la URL por la real.
+    Esto sirve para ocultar los parametros de la url
+
+    if(typeof window.history.pushState == 'function') {
+        window.history.pushState({}, "Hide", "file:///C:/Users/sguerrero/Desktop/dashboardTrianaSat/Sergio/Dashboard%20TrianaSat/publicIndex.html");
+    }*/
+
+    return vars;
+}
+
+$(document).ready(function() {
+    var getParameters = getUrlVars();
+
+    // Se comprueba el tamaño del dispositivo
+    checkScreenSize();
+
+    // En cada cambio de pantalla se comprueba el tamaño del dispositivo.
+    $(window).resize(function() {
+        checkScreenSize();
+    });
+
+    // Marcar como activo el menú mapa en el arranque, y se carga
+    // la página con el mapa.
+    actualMenuSelected = $('#goMapa').closest('li');
+    actualMenuSelected.toggleClass('active');
+
+    $('.main-wrapper').load("sections/sectionMapa.html?token=");
+
+    /******** EVENTOS CLICK MENÚ LATERAL ********/
+
+    // Cambiar el menú activo según se pulsa.
+    $('.menuLink').on('click', function() {
+        actualMenuSelected.toggleClass('active');
+
+        actualMenuSelected = $(this).closest('li');
+        actualMenuSelected.toggleClass('active');
+    });
+
+    $('#goMapa').on('click', function() {
+        $('.main-wrapper').load("sections/sectionMapa.html");
+
+        if (isMobile) {
+            closeSideBar();
+        }
+    });
+
+    $('#goGallery').on('click', function() {
+        var photosURL = 'http://trianasat2-salesianostriana.rhcloud.com/timelapse';
+        var newCols = "";
+
+        $('.main-wrapper').load("sections/sectionGallery.html");
+
+        if (isMobile) {
+            closeSideBar();
+        }
+
+        $.ajax({
+
+            type: "GET",
+            url: photosURL,
+            beforeSend: function(xhr){
+              xhr.setRequestHeader('accept','application/json');
+            },
+            success: function(data) {
+              alert('success');
+
+                var listaFotos = data._embedded.timelapse;
+
+                $.each(listaFotos, function(i, foto) {
+
+                    var photoTimestamp = foto.fecha;
+                    var formattedDate = moment(photoTimestamp).format('DD/MM/YYYY HH:mm:ss');
+
+
+                    var newImageHtml = '<div class="col-lg-3 col-md-4 col-xs-6 thumb"><a class="thumbnail">'+
+                    '<img class="img-responsive" src="'+foto.foto+'" alt="" fecha="'+formattedDate+'"></a>'+
+                    '<button class="js-button btn btn-secondary-outline btn-expand center-block" data-toggle="modal" data-target="#modalPicture"'+
+                    ' type="button" value="Expand photo" role="button"><i class="fa fa-eye" aria-hidden="true"></i></button></div>';
+
+                    newCols += newImageHtml;
+                });
+
+                $('.photosBody').append(newCols);
+
+                setPagination();
+                updateGalleryItems();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert("error ajax");
+                console.log("Error ajax en la petición timelapse");
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+
+
+
+                /*$.each(data, function(i) {
+                    alert(data.text());
+                });*/
+            }
+        });
+    });
+
+    $('#goTable').on('click', function() {
+        $('.main-wrapper').load("sections/sectionTable.html");
+
+        if (isMobile) {
+            closeSideBar();
+        }
+    });
+
+    $('#goMyProfile').on('click', function() {
+        $('.main-wrapper').load("sections/sectionMyProfile.html");
+
+        actualMenuSelected.removeClass('active');
+    });
+
+    /*script que usan los modal al pulsar en siguiente*/
+    $('a[title]').tooltip();
+
+    $(document).on("click", ".cambiarSiguiente", function() {
+        $(".two").parents("li").addClass("active");
+        $(".one").parents("li").removeClass("active");
+    });
+
+    $(document).on("click", ".cambiarTercera", function() {
+        $(".three").parents("li").addClass("active");
+        $(".two").parents("li").removeClass("active");
+    });
+    /*fin script modal*/
+});
